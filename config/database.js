42eaba842a1c848519a068a4dae649895ded4a50
wonderiing <ca223871@gmail.com>
2025-02-21 00:06:28 -0600

    fix

--- config/database.js
+++ config/database.js
@@ -1,40 +1,27 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 
 dotenv.config();
 
-// Configuración por defecto con variables individuales de entorno
-let sequelize;
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        port: process.env.DB_PORT,
+        logging: false,
+
+    }
+)
 
 try {
-    if (process.env.MYSQL_URL) {
-        // Si MYSQL_URL está definida, usamos la URL para conectarnos
-        sequelize = new Sequelize(process.env.MYSQL_URL, {
-            dialect: "mysql",
-            logging: false
-        });
-        console.log("Conexión usando MYSQL_URL exitosa");
-    } else {
-        // Si no se proporciona MYSQL_URL, usamos las variables de entorno tradicionales
-        sequelize = new Sequelize(
-            process.env.DB_NAME,
-            process.env.DB_USER,
-            process.env.DB_PASSWORD,
-            {
-                host: process.env.DB_HOST,
-                dialect: "mysql",
-                port: process.env.DB_PORT || 3306, // Usamos 3306 si DB_PORT no está definida
-                logging: false
-            }
-        );
-        console.log("Conexión usando variables de entorno tradicionales exitosa");
-    }
-
-    // Intentamos autenticar la conexión
     await sequelize.authenticate();
-    console.log("Conexión exitosa");
+    console.log("Conexion exitosa")
 } catch (err) {
-    console.error("Error al conectar:", err);
+    console.error("Error al conectar", err)
 }
 
 export default sequelize;
