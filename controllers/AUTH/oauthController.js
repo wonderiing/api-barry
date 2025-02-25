339048e0e349ae9c9f7fa60c4e20c7a1330a229e
wonderiing <ca223871@gmail.com>
2025-02-25 07:49:27 -0600

    oauth  fix

--- controllers/AUTH/oauthController.js
+++ controllers/AUTH/oauthController.js
@@ -12,45 +12,31 @@ router.get(
 );
 
 router.get(
-    "/auth/google/callback",
-    passport.authenticate("google", { session: false }),
-    async (req, res) => {
-        const { id, name, email } = req.user;
-        let user = await User.findOne({ where: { email } });
-        if (!user) {
-          user = await User.create({
-            name: name,
-            last_name: name,
-            email: email,
-            google_id: id,
-          });
-        }
-    
-      const token = jwt.sign({userId: user.id, email: user.email}, process.env.JWT_SECRET, { expiresIn: "10h" });
-  
-  
-      res.cookie("jwt", token, {
-        httpOnly: true,  
-        secure: true,
-        sameSite: 'none',
-        maxAge: 10 * 60 * 60 * 1000  // 10 horas
+  "/auth/google/callback",
+  passport.authenticate("google", { session: false }),
+  async (req, res) => {
+    const { id, name, email } = req.user;
+    let user = await User.findOne({ where: { email } });
+    if (!user) {
+      user = await User.create({
+        name: name,
+        last_name: name,
+        email: email,
+        google_id: id,
       });
-  
-      // Redirige al frontend
-      res.redirect("https://barry-fs.netlify.app/auth/success");
     }
-  );
-  
 
-router.get('/get-token', (req, res) => {
-    const token = req.cookies.jwt;
+    const token = jwt.sign(
+      { userId: user.id, email: user.email }, 
+      process.env.JWT_SECRET, 
+      { expiresIn: "10h" }
+    );
+
+    // Redirecciona al frontend con el token como parámetro en la URL
+    res.redirect(`http://localhost:5173/auth/success?token=${token}`);
+  }
+);
   
-    if (token) {
-      return res.json({ token });
-    } else {
-      return res.status(403).json({ message: 'No token found' });
-    }
-  });
   
 
 export default router;
